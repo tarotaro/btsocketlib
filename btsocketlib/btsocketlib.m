652265893b4988dedd01c78a973c8531af1fc134
Taro Nakamura <tarotaro57@gmail.com>
2018-10-10 22:02:36 +0900

    Initial Commit

---
+++ btsocketlib/btsocketlib.m
@@ -0,0 +1,13 @@
+//
+//  btsocketlib.m
+//  btsocketlib
+//
+//  Created by 中村太郎 on 2018/10/10.
+//  Copyright © 2018年 中村太郎. All rights reserved.
+//
+
+#import "btsocketlib.h"
+
+@implementation btsocketlib
+
+@end
